8a12858b418ad2ad3f4c71a19c3100654859236c
kaan <kaandesu00@gmail.com>
2024-08-24 18:15:56 +0300

    chore: define the emulator

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import "sync"
+
+type emulator struct {
+	memory          [4096]byte
+	registers       [18]byte
+	pc              uint16
+	stack           [32]uint16
+	sp              uint16
+	I               uint16
+	delayTimer      uint8
+	soundTimer      uint8
+	delayTimerMutex sync.Mutex
+	soundTimerMutex sync.Mutex
+}
+
 func main() {
 }
